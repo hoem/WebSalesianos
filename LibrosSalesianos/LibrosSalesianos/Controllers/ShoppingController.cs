080ef6bfe9a7e8ce703882d9a314165fae8a35fe
Javier Piris Albert <mediomelocoton@gmail.com>
2013-04-16 00:11:50 +0200

    PrimerRepo
    Primera prueba de repo para la web de salesianos

---
+++ LibrosSalesianos/LibrosSalesianos/Controllers/ShoppingController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibrosSalesianos.Models;
+
+namespace LibrosSalesianos.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        //
+        // GET: /Shopping/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShoppingCartExample()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+        public ActionResult AddShoesToCart()
+        {
+            ShoppingCart.Instance.AddItem(1);
+            return RedirectToAction("ShoppingCartExample");
+        }
+
+        public ActionResult AddShirtToCart()
+        {
+            ShoppingCart.Instance.AddItem(2);
+            return RedirectToAction("ShoppingCartExample");
+        }
+
+        public ActionResult AddPantsToCart()
+        {
+            ShoppingCart.Instance.AddItem(3);
+            return RedirectToAction("ShoppingCartExample");
+        }
+    }
+}
---
+++ LibrosSalesianos/LibrosSalesianos/Models/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibrosSalesianos.Models
+{
+    public class Book
+    {
+        public string Image {
+            get { return this.image; }
+            set { this.image = value; }
+        }
+        public string ISBN { 
+            get { return this.isbn; }
+            set { this.isbn = value; }
+        }
+        public string Description { 
+            get { return this.description; }
+            set { this.description = value; }
+        }
+        public float Price { 
+            get { return this.price; }
+            set { this.price = value; }
+        }
+
+        private string isbn;
+        private string description;
+        private float price;
+        private string image;
+
+        public Book(string isbn, string description, float price, string image)
+        {
+            this.isbn = isbn;
+            this.description = description;
+            this.price = price;
+            this.image = image;
+        }
+    }
+}
